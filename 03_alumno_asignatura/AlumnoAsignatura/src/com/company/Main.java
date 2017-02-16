ab7ca0091be9a3ab3c46890ba5f3879bf9d31151
Unknown <tehenua@gmail.com>
2017-02-16 19:06:23 +0100

    editado ambito switch

--- 03_alumno_asignatura/AlumnoAsignatura/src/com/company/Main.java
+++ 03_alumno_asignatura/AlumnoAsignatura/src/com/company/Main.java
@@ -23,46 +23,50 @@ public class Main {
             opcion = Integer.parseInt(br.readLine());
 
             switch (opcion){
-                case 1:
+                case 1: {
                     System.out.print("Introduce el nombre: ");
                     String nombre = br.readLine();
                     System.out.print("Introduce los apellidos: ");
                     String apellidos = br.readLine();
                     System.out.print("Introduce el email: ");
                     String email = br.readLine();
-                    Alumno a = new Alumno(nombre,apellidos,email);
+                    Alumno a = new Alumno(nombre, apellidos, email);
                     alumnos.add(a);
+                }
                     break;
-                case 2:
+                case 2: {
                     System.out.print("Introduce el nombre: ");
-                    String nombreA = br.readLine();
+                    String nombre = br.readLine();
                     System.out.print("Introduce las horas semanales: ");
                     int horas = Integer.parseInt(br.readLine());
-                    Asignatura as = new Asignatura(nombreA,horas);
-                    asignaturas.add(as);
+                    Asignatura a = new Asignatura(nombre, horas);
+                    asignaturas.add(a);
+                }
                     break;
-                case 3:
+                case 3: {
                     int alumno = elegirAlumno(alumnos, br);
                     int asignatura = elegirAsignatura(asignaturas, br);
                     alumnos.get(alumno).addAsignatura(asignaturas.get(asignatura));
+                }
                     break;
-                case 4:
-                    int asig = elegirAsignatura(asignaturas, br);
-                    System.out.println("Alumnos matriculados en "+asignaturas.get(asig).getNombre());
-                    for(Alumno alu: asignaturas.get(asig).getAlumnos()){
-                        System.out.println(alu.getNombre()+" "+alu.getApellidos());
+                case 4: {
+                    int asignatura = elegirAsignatura(asignaturas, br);
+                    System.out.println("Alumnos matriculados en " + asignaturas.get(asignatura).getNombre());
+                    for (Alumno alu : asignaturas.get(asignatura).getAlumnos()) {
+                        System.out.println(alu.getNombre() + " " + alu.getApellidos());
                     }
+                }
                     break;
-                case 5:
-
-                    int alumInt = elegirAlumno(alumnos, br);
-                    System.out.println("Asignaturas en las que está matriculado "+alumnos.get(alumInt).getNombre()+" "+alumnos.get(alumInt).getApellidos());
-                    int totalHoras=0;
-                    for(Asignatura asignat: alumnos.get(alumInt).getAsignaturas()){
+                case 5: {
+                    int alumno = elegirAlumno(alumnos, br);
+                    System.out.println("Asignaturas en las que está matriculado " + alumnos.get(alumno).getNombre() + " " + alumnos.get(alumInt).getApellidos());
+                    int totalHoras = 0;
+                    for (Asignatura asignat : alumnos.get(alumno).getAsignaturas()) {
                         System.out.println(asignat.getNombre());
                         totalHoras += asignat.getHorasSemanales();
                     }
-                    System.out.println("Total horas: "+totalHoras);
+                    System.out.println("Total horas: " + totalHoras);
+                }
                     break;
                 case 6:
                     System.out.println("Adios");
@@ -74,18 +78,18 @@ public class Main {
     public static int elegirAlumno(List<Alumno> alumnos, BufferedReader br) throws IOException {
         System.out.println("Elige un alumno:");
         for (int i = 0; i < alumnos.size(); i++) {
-            System.out.println(i+" "+alumnos.get(i).getNombre());
+            System.out.println(i+1+" "+alumnos.get(i).getNombre());
         }
-        int alumno = Integer.parseInt(br.readLine());
+        int alumno = Integer.parseInt(br.readLine())-1;
         return alumno;
     }
 
     public static int elegirAsignatura(List<Asignatura> asignaturas,BufferedReader br ) throws IOException {
         System.out.println("Elige una asignatura:");
         for (int i = 0; i < asignaturas.size(); i++) {
-            System.out.println(i+" "+asignaturas.get(i).getNombre());
+            System.out.println(i+1+" "+asignaturas.get(i).getNombre());
         }
-        int asignatura = Integer.parseInt(br.readLine());
+        int asignatura = Integer.parseInt(br.readLine())-1;
         return asignatura;
     }
 }
